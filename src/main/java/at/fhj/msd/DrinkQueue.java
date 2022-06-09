de0a5ad5e34ea007f4067f6e83c140338ba12d3a
AndreasSchweighofer <102227756+AndreasSchweighofer@users.noreply.github.com>
2022-06-09 13:53:49 +0200

    New Branch for drinks implementation and implementing method offer()

---
+++ src/main/java/at/fhj/msd/DrinkQueue.java
@@ -0,0 +1,30 @@
+package at.fhj.msd;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class DrinkQueue implements IQueue {
+
+    private List<Drink> elements = new ArrayList<Drink>();
+    private int maxSize = 5;
+
+    public DrinkQueue(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    @Override
+    public boolean offer(String obj) {
+        if (elements.size() != maxSize) {
+            switch (obj) {
+                case "Andys Stress Cocktail" -> elements.add(new SimpleDrink(obj, new Liquid("Marillen-Schnaps", 0.5, 0.9)));
+                case "Nichts für Humorlose" -> elements.add(new SimpleDrink(obj, new Liquid("Gin", 0.2, 0.3)));
+                case "Suffering Bastard" -> elements.add(new SimpleDrink(obj, new Liquid("Whiskey", 0.2, 0.6)));
+            }
+        }
+        else
+            return false;
+
+        return true;
+    }
+}
