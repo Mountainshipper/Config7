96257e514e5f3b76b472c4b3401e301a298023b6
Samuel Spencer <samuel.spencer@edu.fh-joanneum.at>
2022-06-09 15:56:57 +0200

    Test

--- src/test/java/at/fhj/msd/DrinkTest.java
+++ src/test/java/at/fhj/msd/DrinkTest.java
@@ -1,9 +1,16 @@
 package at.fhj.msd;
 
 import junit.framework.TestCase;
+import org.junit.jupiter.api.BeforeEach;
 
 public class DrinkTest extends TestCase {
 
+   
 
+        private DrinkQueue calc;
+        @BeforeEach
+        public void setup(){
+            calc = new DrinkQueue(5);
+        }
 
 }
